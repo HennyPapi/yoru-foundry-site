575b4e98e74cf76cc662ff2d105a686a8b6c49b8
HennyPapi <mllerenafinances@gmail.com>
2026-09-14 23:45:37 -0400

    Restrict grain test to the homepage hero

--- build.js
+++ build.js
@@ -2,7 +2,7 @@ const fs = require("node:fs");
 const path = require("node:path");
 
 const config = Object.freeze({
-  stylesheetVersion: "yn-design-p3-5",
+  stylesheetVersion: "yn-design-p3-5a",
   siteTitle: "Yoru Foundry",
   SITE_MODE: "prelaunch",
 });
